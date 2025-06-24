332780aa96e8fe31b807aef0bd00cfdc2350a2a4
SanaHameed2 <sana.faiza2@gmail.com>
2025-06-24 18:23:17 +0500

    first commit

---
+++ Variable-names/app.js
@@ -0,0 +1,8 @@
+// Task 3: Rules for Naming JS Variables
+document.writeln("<h2>Task 3: Rules for Naming JavaScript Variables</h2>");
+document.writeln("<p style='font-size: 30px; font-weight: bold; color: blue;'>Variable names can only contain letters, numbers, $ and _ <br>For example: $my_1stVariable</p>");
+document.writeln("<p style='font-size: 30px; font-weight: bold; color: blue;'>Variables must begin with a letter, $ or _ <br>For example: $name, _name or name</p>");
+document.writeln("<p style='font-size: 30px; font-weight: bold; color: blue;'>Variable names are case sensitive</p>");
+document.writeln("<p style='font-size: 30px; font-weight: bold; color: blue;'>Variable names should not be JavaScript keywords</p>");
+document.writeln("<p style='color: gray;'>— End of Task —</p>");
+document.writeln("<hr>");
